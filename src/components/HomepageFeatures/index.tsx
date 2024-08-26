96726174d21e35bcfffee86756527182bed246a3
atupenda <catupenda@gmail.com>
2024-08-26 13:11:26 +0300

    add index.tsx

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -9,19 +9,17 @@ type FeatureItem = {
 };
 
 const FeatureList: FeatureItem[] = [
+  
+   // title: 'Easy to Use',
+    //Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    //description: (
+     // <>
+       // Docusaurus was designed from the ground up to be easily installed and
+       // used to get your website up and running quickly.
+    //
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'my afya msafiri',
+    Svg: require('static/img/logo.svg').default,
     description: (
       <>
         Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
@@ -29,16 +27,16 @@ const FeatureList: FeatureItem[] = [
       </>
     ),
   },
-  {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
+  //{
+   // title: 'Powered by React',
+   // Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    //description: (
+      //<>
+       // Extend or customize your website layout by reusing React. Docusaurus can
+       // be extended while reusing the same header and footer.
+    //  </>
+   // ),
+ // },
 ];
 
 function Feature({title, Svg, description}: FeatureItem) {
