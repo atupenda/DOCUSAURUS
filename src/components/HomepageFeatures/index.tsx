61e8dea17943ce68b35a57171828831c3bf7302b
atupenda <catupenda@gmail.com>
2024-08-29 09:42:57 +0300

    add index.tsx

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -17,26 +17,26 @@ const FeatureList: FeatureItem[] = [
        // Docusaurus was designed from the ground up to be easily installed and
        // used to get your website up and running quickly.
     //
-  {
-    title: 'my afya msafiri',
-    Svg: require('@site/static/img/logo.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-},
-  
-   // title: 'Powered by React',
-   // Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    //description: (
+  //{
+   // title: 'my afya msafiri',
+   // Svg: require('@site/static/img/logo.svg').default,
+   // description: (
       //<>
-       // Extend or customize your website layout by reusing React. Docusaurus can
-       // be extended while reusing the same header and footer.
-    //  </>
-   // ),
- // },
+      //  user guide for electronic screening of travelers and public health events at points of entry<code>docs</code> directory.
+     // </>
+    //),
+//},
+  //(
+   // title: ' My Afya Msafiri',
+    //Svg: require('@site/static/img/logo.svg').default,
+    //description: 
+   // (
+     // <>
+     //  Extend or customize your website layout by reusing React. Docusaurus can
+      //be extended while reusing the same header and footer.
+     //</>
+//),
+//},
 ];
 
 function Feature({title, Svg, description}: FeatureItem) {
